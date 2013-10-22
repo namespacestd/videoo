fd324037f13baf9f08eaa067d9f6bf936113a19e
mattmeisinger <mrm2205@columbia.edu>
2013-10-22 08:57:37 -0400

    Added user ratings to Movie Details page. Handled logged in users and anonymous.

--- movie/urls.py
+++ movie/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
 
     # movie detail url: /movie/detail/{id}
     url(r'^detail/(?P<id>\d+)/+$', views.detail, name='detail'),
+    url(r'^rate/(?P<id>\d+)/+$', views.rate, name='rate'),
     url(r'^search/+.*$', views.search, name='search'),
 
     #url(r'^logout$', views.logout, name='logout'),
