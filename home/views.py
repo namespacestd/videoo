35db0caf882793cd8e586bbda8ffc0c485c17dfc
Martin Li <mcl2159@columbia.edu>
2013-10-25 15:19:28 -0400

    put login and signup forms on same page, will mess with ajax later to do error messages

--- home/views.py
+++ home/views.py
@@ -3,11 +3,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
+from ase1.models import CreateAccountForm
 
 
 def index(request):
     return render(request, 'main/index.html', {
-        'form': AuthenticationForm(),
+        'login_form': AuthenticationForm(),
+        'signup_form': CreateAccountForm(),
         'username': request.user.username,
         'message': 'Get',
         'logged_in_message': 'Current Username: %s' % request.user.username,
