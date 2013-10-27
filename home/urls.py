04223161e60f93a7edcf07655aed68e6aae64cff
Matthew Suozzo <matthew.suozzo@gmail.com>
2013-10-26 20:06:39 -0400

    PEP compliance

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,4 @@
 __author__ = 'matt'
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('',
-    url(r'^login$', views.login, name='login'),
-    url(r'^logout$', views.logout, name='logout'),
-    url(r'^create$', views.create, name='create'),
-)
+urlpatterns = patterns('',)
