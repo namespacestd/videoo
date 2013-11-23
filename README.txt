69c89c7e428e1113b7b65e9117c69aa63dbc0489
mattmeisinger <mrm2205@columbia.edu>
2013-11-23 08:46:25 -0500

    Took API key out of the settings file (it is an old API key anyway). Added readme.

---
+++ README.txt
@@ -0,0 +1,12 @@
+Videe-o
+=======
+
+This is a class project for Advanced Software Engineering.
+
+To Run
+------
+
+1) Install python and django on your machine
+2) Install PIP, and prerequisites using: 'pip install -r requirements.txt'
+3) Enter your TMDB API key into the file '/movie/settings.py'
+4) Start the site using 'python manage.py runserver'
--- movie/settings.py
+++ movie/settings.py
@@ -27,6 +27,13 @@ def __gen_base_url():
     logger.debug('Base URL: %s', new_base_url)
     return new_base_url
 
-# Fields
-api_key = 'f93d4374e57a40fd7cc1028e0c95ad9f'
+# The api key to use for the TMDB to work.  This needs to be set before the
+# application can work.
+api_key = ''
+
+if not api_key:
+    raise Exception('Unable to find API key in "/movie/settings.py".'
+    + ' An account must be created and a TMDB API key acquired before this application can run.')
+
+# The base url for TMDB images
 base_url = __gen_base_url()
